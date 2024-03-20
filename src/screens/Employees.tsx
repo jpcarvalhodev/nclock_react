cc07cbb12332afc3afc9917c3bdba2fff2312041
JPCarvalho23 <135696827+JPCarvalho23@users.noreply.github.com>
2024-03-20 16:43:58 +0000

    commit

--- src/screens/Employees.tsx
+++ src/screens/Employees.tsx
@@ -1,7 +1,11 @@
+import { Footer } from "../components/Footer";
 import { NavBar } from "../components/NavBar";
+
 
 export const Employees = () => {
     return (
-        <NavBar />
+        <div>
+            <NavBar />
+        </div>
     );
 }
