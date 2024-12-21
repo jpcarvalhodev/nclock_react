d07c1e48102a58c2a214dc843ce01ced862773b5
João Pedro Carvalho <joaopedro_2311@hotmail.com>
2024-12-21 08:29:00 +0000

    Update NewUsers.tsx

--- src/pages/configs/NewUsers.tsx
+++ src/pages/configs/NewUsers.tsx
@@ -216,9 +216,7 @@ export const NewUsers = () => {
                     placement="top"
                     overlay={<Tooltip className="custom-tooltip">Apagar</Tooltip>}
                 >
-                    <Button className='delete-button' variant="outline-danger" onClick={() => handleOpenDeleteModal(row.id)} >
-                        <i className="bi bi-trash-fill"></i>
-                    </Button>
+                    <CustomOutlineButton className="action-button" icon='bi bi-trash-fill' onClick={() => handleOpenDeleteModal(row.id)} />
                 </OverlayTrigger>
             </div>
         ),
