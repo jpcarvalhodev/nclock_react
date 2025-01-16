4c4f2839c8f117d90a83c4f44911d5dea660f88e
João Pedro Carvalho <135696827+jpcarvalhodev@users.noreply.github.com>
2025-01-16 10:30:26 +0000

    Update UpdateModalDeptGrp.tsx

--- src/modals/UpdateModalDeptGrp.tsx
+++ src/modals/UpdateModalDeptGrp.tsx
@@ -8,8 +8,8 @@ import { toast } from 'react-toastify';
 import { CustomOutlineButton } from '../components/CustomOutlineButton';
 import { customStyles } from '../components/CustomStylesDataTable';
 import { usePersons } from '../context/PersonsContext';
-import { employeeFields } from '../fields/Fields';
-import { Department, Employee, Group } from '../types/Types';
+import { employeeFields } from '../helpers/Fields';
+import { Department, Employee, Group } from '../helpers/Types';
 
 import { CreateModalEmployees } from './CreateModalEmployees';
 import { UpdateModalEmployees } from './UpdateModalEmployees';
